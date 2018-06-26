a48f54cd55114776523983730155a680677cbac8
Ivan Shvetsov <>
2018-06-26 11:02:11 +0500

    Clean up code

--- src/nibbles.py
+++ src/nibbles.py
@@ -1,16 +1,7 @@
 import pyglet
 
-window = pyglet.window.Window()
+game_window = pyglet.window.Window()
 
-label = pyglet.text.Label('Hello, world',
-  font_name='Times New Roman',
-  font_size=36,
-  x=window.width//2, y=window.height//2,
-  anchor_x='center', anchor_y='center')
+if __name__ == '__main__':
+    pyglet.app.run()
 
-@window.event
-def on_draw():
-    window.clear()
-    label.draw()
-
-pyglet.app.run()
